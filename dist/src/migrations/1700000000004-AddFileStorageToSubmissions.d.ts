569b90ca90bc053b1febabb6b1cee1ad9e6dde2e
vishwjeet <info.vishwjeetkumar.com>
2025-10-12 22:18:39 +0530

    Remove dist/ folder from git tracking and add to .gitignore

--- dist/src/migrations/1700000000004-AddFileStorageToSubmissions.d.ts
+++
@@ -1,5 +0,0 @@
-import { MigrationInterface, QueryRunner } from 'typeorm';
-export declare class AddFileStorageToSubmissions1700000000004 implements MigrationInterface {
-    up(queryRunner: QueryRunner): Promise<void>;
-    down(queryRunner: QueryRunner): Promise<void>;
-}
