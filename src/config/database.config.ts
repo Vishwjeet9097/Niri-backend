0a5d7de8de3d7734a7df63da60b24c8251be7222
vishwjeet <info.vishwjeetkumar.com>
2025-10-12 21:58:16 +0530

    issue fixed

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -15,8 +15,8 @@ export class DatabaseConfig implements TypeOrmOptionsFactory {
       type: "postgres",
       host: this.configService.get("DB_HOST"),
       port: parseInt(this.configService.get("DB_PORT") || "5432", 10),
-      username: this.configService.get("DB_USER"),
-      password: this.configService.get("DB_PASS"),
+      username: this.configService.get("DB_USERNAME"),
+      password: this.configService.get("DB_PASSWORD"),
       database: this.configService.get("DB_NAME"),
       ssl: { rejectUnauthorized: false },
       extra: {
