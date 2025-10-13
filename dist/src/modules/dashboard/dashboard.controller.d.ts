7b6a98a5e533799e067bdb0a69b32f7214751698
vishwjeet <info.vishwjeetkumar.com>
2025-10-13 13:22:42 +0530

    Add .gitignore and remove dist folder from tracking - complete dist/ folder ignored

--- dist/src/modules/dashboard/dashboard.controller.d.ts
+++
@@ -1,8 +0,0 @@
-import { DashboardService } from './dashboard.service';
-export declare class DashboardController {
-    private readonly dashboardService;
-    constructor(dashboardService: DashboardService);
-    getSummary(req: any): Promise<import("./dashboard.service").DashboardSummary>;
-    getKPIs(req: any): Promise<any>;
-    getRecentActivities(req: any): Promise<any[]>;
-}
