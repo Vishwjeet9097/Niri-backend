3a567b969471749bf42b3b650d6fae501d64ea28
Your Name <mohd.fayeem@mounttalent.com>
2025-10-13 16:49:48 +0530

    added comment logic

--- dist/src/middleware/audit.middleware.d.ts
+++
@@ -1,8 +0,0 @@
-import { NestMiddleware } from '@nestjs/common';
-import { Request, Response, NextFunction } from 'express';
-import { ClsService } from 'nestjs-cls';
-export declare class AuditMiddleware implements NestMiddleware {
-    private clsService;
-    constructor(clsService: ClsService);
-    use(req: Request, res: Response, next: NextFunction): void;
-}
