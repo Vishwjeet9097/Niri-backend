0514cd2e605c6c14f9a9d37e45809dd7da5e4b7d
vishwjeet <info.vishwjeetkumar.com>
2025-10-22 13:21:34 +0530

    changes

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,10 +1,6 @@
 import { Injectable } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
 import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from "@nestjs/typeorm";
-import { User } from "../entities/user.entity";
-import { Submission } from "../entities/submission.entity";
-import { AuditLog } from "../entities/audit-log.entity";
-import { FinalScore } from "../entities/final-score.entity";
 
 @Injectable()
 export class DatabaseConfig implements TypeOrmOptionsFactory {
