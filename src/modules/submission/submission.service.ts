c8bd82b6b14f32a60d834c0716ec34e50b8239d1
harsh-gupta-skillmine <harsh-gupta@skill-mine.com>
2025-12-03 15:39:18 +0530

    scope.isactive issue fixed

--- src/modules/submission/submission.service.ts
+++ src/modules/submission/submission.service.ts
@@ -181,10 +181,10 @@ export class SubmissionService {
       .createQueryBuilder("scope")
       .leftJoinAndSelect("scope.indicator", "indicator")
       .where("scope.userId = :userId", { userId })
-      .andWhere("scope.isActive = :isActive", { isActive: true })
-      .andWhere("indicator.isActive = :indicatorActive", {
-        indicatorActive: true,
-      })
+      // .andWhere("scope.isActive = :isActive", { isActive: true })
+      // .andWhere("indicator.isActive = :indicatorActive", {
+      //   indicatorActive: true,
+      // })
       .getMany();
 
     return userIndicatorScopes.map((scope) => scope.indicator.code);
