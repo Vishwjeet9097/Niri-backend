99a09fb90c9d1fc26a80a24b21586a8c1fe15149
ajay-skillmine <ajay.k@skill-mine.com>
2025-12-11 16:13:23 +0530

    Migration

--- src/migrations/1765431826767-CreateUlbMasterTable.ts
+++ src/migrations/1765431826767-CreateUlbMasterTable.ts
@@ -14,12 +14,10 @@ export class CreateUlbMasterTable1765431826767 implements MigrationInterface {
         `);
         
         await queryRunner.query(`CREATE TABLE "ulb_masters" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "state_name" character varying(255) NOT NULL, "city_name" character varying(255) NOT NULL, "ulb_name" character varying(255), "ulb_type" character varying(255), "status" "public"."ulb_masters_status_enum" NOT NULL DEFAULT '1', "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_c605169bd8a6e91f8758cb9315a" PRIMARY KEY ("id"))`);
-        await queryRunner.query(`ALTER TABLE "submissions" ALTER COLUMN "attached_files" SET DEFAULT '[]'::jsonb`);
-    }
+     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "submissions" ALTER COLUMN "attached_files" SET DEFAULT '[]'`);
-        await queryRunner.query(`DROP TABLE "ulb_masters"`);
+         await queryRunner.query(`DROP TABLE "ulb_masters"`);
         await queryRunner.query(`DROP TYPE "public"."ulb_masters_status_enum"`);
     }
 
