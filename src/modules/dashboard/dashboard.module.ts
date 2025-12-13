a3ef5df56d5f175cb752141ceebd96977dff47a6
ajay-skillmine <ajay.k@skill-mine.com>
2025-12-13 15:52:46 +0530

    dashboard services added

--- src/modules/dashboard/dashboard.module.ts
+++ src/modules/dashboard/dashboard.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Submission } from '../../entities/submission.entity';
 import { DashboardService } from './dashboard.service';
 import { DashboardController } from './dashboard.controller';
+import { Indicator } from '@/entities/indicator.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Submission])],
+  imports: [TypeOrmModule.forFeature([Submission, Indicator])],
   controllers: [DashboardController],
   providers: [DashboardService],
   exports: [DashboardService],
